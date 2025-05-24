2e7df3e2ad3eec673d890c31240bfd1ea4d2951f
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-24 14:14:23 -0500

    Started Arrays video 06

--- Arrays.UI.Console/Program.cs
+++ Arrays.UI.Console/Program.cs
@@ -8,7 +8,7 @@ array.Fill(1, 100);
 Console.WriteLine("Unsorted array:\n");
 Console.WriteLine(array);
 
-array.Sort();
+array.Sort(true);
 
 Console.WriteLine("Sorted array:\n");
 Console.WriteLine(array);
